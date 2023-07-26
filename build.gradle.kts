5b94818c21aa678f1af317b1f409b46435584908
Gomzyakov I. A <76881688+psajd@users.noreply.github.com>
2023-07-26 22:13:13 +0300

    feat: add logger
    add lib ch.qos.logback:logback-classic for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation("com.github.seratch:notion-sdk-jvm-core:1.8.0")
     implementation("dev.inmo:tgbotapi:7.0.1")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+    implementation("org.slf4j:slf4j-api:1.7.32")
+    implementation("ch.qos.logback:logback-classic:1.2.9")
 }
 
 tasks.test {
@@ -21,7 +24,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
 
 application {
